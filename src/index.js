8b5448536340340a87d5d80ecb9bb2d0e7f893c6
Agapitova Ksenia <kagapitova@inbox.ru>
2023-02-08 22:56:59 +0300

    add: solution of brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrPair = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arrPair.push(bracketsConfig[i][0] + bracketsConfig[i][1])
+    }
+
+    while (true) {
+        let counter = 0;
+        for (let j = 0; j < arrPair.length; j++) {
+            if (str.includes(arrPair[j])) {
+                str = str.replace(arrPair[j], '')
+                counter++
+            }
+        }
+        if (counter === 0) {
+            break;
+        }
+    }
+    return str.length === 0;
 }
